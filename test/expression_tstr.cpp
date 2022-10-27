501d18737840d293cf8dcb2f4af10281b86160ed
Kate <kate@nil.foundation>
2022-10-27 18:20:22 +0300

    updated expression_tstr tests #5

--- test/expression_tstr.cpp
+++ test/expression_tstr.cpp
@@ -46,29 +46,129 @@ using namespace nil::crypto3::math::expressions_tstr;
 typedef fields::bls12_fr<381> FieldType;
 
 BOOST_AUTO_TEST_SUITE(expression_test_suite)
-////
-// BOOST_AUTO_TEST_CASE(expression_expression_0) {
 //
-//     constexpr const char *v1 = "v1";
-//     constexpr const char *v0 = "v0";
-//
-//     FieldType::value_type p0 = FieldType::value_type(5);
-//     FieldType::value_type p1 = FieldType::value_type(15);
-//
-//     FieldType::value_type p2 = FieldType::value_type(5) * p0 + p1 / p0;
-//
-//     std::vector<const char *> s = {v0, v1};
-//     std::vector<FieldType::value_type> ps = {p0, p1};
-//     auto dictionary = std::make_pair(s, ps);
-//     typedef decltype("5 * v0 + v1 / v0"_tstr) expr_str;
-//     auto expr_type = ExpressionTstr<FieldType::value_type, X<expr_str>>();
-//    // expr_type expr();
-//     FieldType::value_type c = expr_type.eval(dictionary);
-//    // FieldType::value_type c1 = expr->eval(dictionary);
-//     //    for (std::size_t i = 0; i < c.size(); i++) {
-//     BOOST_CHECK_EQUAL(c.data, p2.data);
-//     // }
-// }
+ BOOST_AUTO_TEST_CASE(expression_expression) {
+
+     constexpr const char *v1 = "v1";
+     constexpr const char *v0 = "v0";
+
+     FieldType::value_type p0 = FieldType::value_type(5);
+     FieldType::value_type p1 = FieldType::value_type(15);
+
+     FieldType::value_type p2 = p0 + p1;
+
+     std::vector<const char *> s = {v0, v1};
+     std::vector<FieldType::value_type> ps = {p0, p1};
+
+     auto dictionary = std::make_pair(s, ps);
+
+     typedef X<decltype("v0 + v1"_tstr)> expr_str;
+
+     FieldType::value_type c =  ExpressionTstr<expr_str>::eval(dictionary);
+
+     BOOST_CHECK_EQUAL(c.data, p2.data);
+
+ }
+
+ BOOST_AUTO_TEST_CASE(expression_expression_0) {
+
+     constexpr const char *v1 = "v1";
+     constexpr const char *v0 = "v0";
+
+     FieldType::value_type p0 = FieldType::value_type(5);
+     FieldType::value_type p1 = FieldType::value_type(15);
+
+     FieldType::value_type p2 = p0 - p1;
+
+     std::vector<const char *> s = {v0, v1};
+     std::vector<FieldType::value_type> ps = {p0, p1};
+
+     auto dictionary = std::make_pair(s, ps);
+
+     typedef X<decltype("v0 - v1"_tstr)> expr_str;
+
+     FieldType::value_type c =  ExpressionTstr<expr_str>::eval(dictionary);
+
+     BOOST_CHECK_EQUAL(c.data, p2.data);
+
+ }
+
+ BOOST_AUTO_TEST_CASE(expression_expression_1) {
+
+     constexpr const char *v1 = "v1";
+     constexpr const char *v0 = "v0";
+
+     FieldType::value_type p0 = FieldType::value_type(5);
+     FieldType::value_type p1 = FieldType::value_type(15);
+
+     FieldType::value_type p2 = p0 * p1;
+
+     std::vector<const char *> s = {v0, v1};
+     std::vector<FieldType::value_type> ps = {p0, p1};
+
+     auto dictionary = std::make_pair(s, ps);
+
+     typedef X<decltype("v0 * v1"_tstr)> expr_str;
+
+     FieldType::value_type c =  ExpressionTstr<expr_str>::eval(dictionary);
+
+     BOOST_CHECK_EQUAL(c.data, p2.data);
+
+ }
+
+ BOOST_AUTO_TEST_CASE(expression_expression_2) {
+
+     constexpr const char *v1 = "v1";
+     constexpr const char *v0 = "v0";
+
+     FieldType::value_type p0 = FieldType::value_type(5);
+     FieldType::value_type p1 = FieldType::value_type(15);
+
+     FieldType::value_type p2 = p1 / p0;
+
+     std::vector<const char *> s = {v0, v1};
+     std::vector<FieldType::value_type> ps = {p0, p1};
+
+     auto dictionary = std::make_pair(s, ps);
+
+     typedef X<decltype("v1 / v0"_tstr)> expr_str;
+
+     FieldType::value_type c = ExpressionTstr<expr_str>::eval(dictionary);
+
+     BOOST_CHECK_EQUAL(c.data, p2.data);
+
+ }
+
+ BOOST_AUTO_TEST_CASE(expression_polynomial_expression_1) {
+     //
+     constexpr const char *v1 = "v1";
+     constexpr const char *v0 = "v0";
+     //
+     polynomial<FieldType::value_type> p0 = {5, 0, 0, 13, 0, 1};
+     polynomial<FieldType::value_type> p1 = {13, 0, 1};
+     // auto x =p0.evaluate(FieldType::value_type);
+     // std::cout<<x.data<<std::endl;
+     polynomial<FieldType::value_type> p2;
+     p2 = p0 + p1 / p0;
+
+     std::vector<const char *> s = {v0, v1};
+     std::vector<polynomial<FieldType::value_type>> ps = {p0, p1};
+     auto dictionary = std::make_pair(s, ps);
+     auto res = ExpressionTstr<X<decltype("v0 + v1 / v0"_tstr)>>::eval(dictionary);
+
+
+     for (std::size_t i = 0; i < res.size(); i++) {
+         BOOST_CHECK_EQUAL(res[i].data, p2[i].data);
+     }
+
+     FieldType::value_type val0 = FieldType::value_type(5);
+     FieldType::value_type val1 = FieldType::value_type(15);
+     auto val2 = val0 + val1 / val0;
+     std::vector<FieldType::value_type> vals = {val0, val1};
+     auto dictionary2 = std::make_pair(s, vals);
+     auto res2 = ExpressionTstr<X<decltype("v0 + v1 / v0"_tstr)>>::eval(dictionary2);
+     BOOST_CHECK_EQUAL(res2.data, val2.data);
+ }
 // BOOST_AUTO_TEST_CASE(expression_expression_1) {
 //
 //     constexpr const char *v1 = "v1";
@@ -128,36 +228,7 @@ BOOST_AUTO_TEST_SUITE(expression_test_suite)
 //     FieldType::value_type c = expr_type::eval(dictionary);
 //     BOOST_CHECK_EQUAL(c.data, p2.data);
 // }
-BOOST_AUTO_TEST_CASE(expression_polynomial_expression_1) {
-    //
-    constexpr const char *v1 = "v1";
-    constexpr const char *v0 = "v0";
-    //
-    polynomial<FieldType::value_type> p0 = {5, 0, 0, 13, 0, 1};
-    polynomial<FieldType::value_type> p1 = {13, 0, 1};
-    // auto x =p0.evaluate(FieldType::value_type);
-    // std::cout<<x.data<<std::endl;
-    polynomial<FieldType::value_type> p2;
-    p2 = p0 + p1 / p0;
 
-    std::vector<const char *> s = {v0, v1};
-    std::vector<polynomial<FieldType::value_type>> ps = {p0, p1};
-    auto dictionary = std::make_pair(s, ps);
-    auto res = ExpressionTstr<X<decltype("v0 + v1 / v0"_tstr)>>::eval(dictionary);
-
-
-    for (std::size_t i = 0; i < res.size(); i++) {
-        BOOST_CHECK_EQUAL(res[i].data, p2[i].data);
-    }
-
-    FieldType::value_type val0 = FieldType::value_type(5);
-    FieldType::value_type val1 = FieldType::value_type(15);
-    auto val2 = val0 + val1 / val0;
-    std::vector<FieldType::value_type> vals = {val0, val1};
-    auto dictionary2 = std::make_pair(s, vals);
-    auto res2 = ExpressionTstr<X<decltype("v0 + v1 / v0"_tstr)>>::eval(dictionary2);
-    BOOST_CHECK_EQUAL(res2.data, val2.data);
-}
 //
 // BOOST_AUTO_TEST_CASE(expression_expression_4) {
 //
