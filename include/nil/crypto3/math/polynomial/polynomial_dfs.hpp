50ef387f93e09f35467bc2e71699bf0c84fe274f
Martun Karapetyan <martun.karapetyan@gmail.com>
2023-05-26 08:08:33 -0700

    Fixing the power function for polynomial_dfs.

--- include/nil/crypto3/math/polynomial/polynomial_dfs.hpp
+++ include/nil/crypto3/math/polynomial/polynomial_dfs.hpp
@@ -627,14 +627,14 @@ namespace nil {
                     return tmp;
                 }
 
-                polynomial_dfs pow(const FieldValueType& alpha) {
-                    if (alpha == 1) {
+                polynomial_dfs pow(int power) {
+                    if (power == 1) {
                         return *this;
                     }
-                    FieldValueType half = alpha / 2;
+                    int half = power / 2;
                     polynomial_dfs r = this->pow(half);
                     r *= r;
-                    if (alpha != half + half) {
+                    if (power != half + half) {
                         r *= *this;
                     }
                     return r;
